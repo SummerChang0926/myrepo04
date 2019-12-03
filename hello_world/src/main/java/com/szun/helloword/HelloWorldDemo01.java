1a8232cdd8ff51d18a42e6cafedf76a5ebef85dd
SummerChang <1421539153@qq.com>
2019-12-03 12:31:03 +0800

    第一次

---
+++ hello_world/src/main/java/com/szun/helloword/HelloWorldDemo01.java
@@ -0,0 +1,10 @@
+package com.szun.helloword;
+
+import org.junit.Test;
+
+public class HelloWorldDemo01 {
+    @Test
+    public void method() {
+        System.out.println("hello world");
+    }
+}
